3b5aa3ea7cbb20c83b4d4a6c03046b3372480d83
cjhillbrand-mac <cjhillbrand@gmail.com>
2021-10-10 19:26:00 -0700

    restructure for jvm

---
+++ runtime_lib/tests/class_loader_integration_tests.rs
@@ -0,0 +1,14 @@
+use runtime_lib::class_loaders::class_loader::ClassLoader;
+use runtime_lib::class_loaders::system_class_loader::SystemClassLoader;
+use runtime_lib::entities::class_struct::ClassStruct;
+
+#[test]
+#[ignore]
+fn system_class_loader_loads_class_in_current_directory() {
+	let file: &str = "../resources/Example1";
+	let loader: SystemClassLoader = SystemClassLoader {};
+
+	let result: ClassStruct = loader.load_class(file);
+
+	println!("{:#?}", result)
+}
